cef7f44c55b59b541b51f5082ace2b794725b8e9
dave <dxmann73@gmail.com>
2017-10-07 09:33:20 +0200

    feat: fetch books from actual BooksAPI instead of static pre-set state
    Fetch the static book data from the API now. Authors is passed an array
    now, which seems fitting. Also the imageURL has moved and width/height
    properties are gone. Since they are mostly the same, we just set fixed
    sizes for now.
    Also, since we don't have control over the API, I've renamed shelfIf
    back to 'shelf'.

--- src/BookComponent.js
+++ src/BookComponent.js
@@ -4,28 +4,29 @@ import PropTypes from 'prop-types';
 class BookComponent extends Component {
     static propTypes = {
         book: PropTypes.shape({
-            width: PropTypes.number.isRequired,
-            height: PropTypes.number.isRequired,
-            backgroundImage: PropTypes.string,
-            shelfId: PropTypes.string.isRequired,
+            shelf: PropTypes.string.isRequired,
             title: PropTypes.string.isRequired,
-            authors: PropTypes.string.isRequired
+            authors: PropTypes.array.isRequired, // array of strings
+            imageLinks: PropTypes.shape({
+                smallThumbnail: PropTypes.string.isRequired,
+                thumbnail: PropTypes.string.isRequired
+            })
         }),
         triggerMoveToShelf: PropTypes.func.isRequired
     };
 
     render = () => {
-        let {height, width, backgroundImage, shelfId, title, authors} = this.props.book;
+        let {shelf, title, authors, imageLinks} = this.props.book;
 
         return <div className="book">
             <div className="book-top">
                 <div className="book-cover" style={{
-                    width: width,
-                    height: height,
-                    backgroundImage: backgroundImage
+                    width: 128,
+                    height: 190,
+                    backgroundImage: `url("${imageLinks.smallThumbnail}")`
                 }}></div>
                 <div className="book-shelf-changer">
-                    <select defaultValue={shelfId} onChange={(event) => this.props.triggerMoveToShelf(event.target.value)}>
+                    <select defaultValue={shelf} onChange={(event) => this.props.triggerMoveToShelf(event.target.value)}>
                         <option value="none" disabled>Move to...</option>
                         <option value="currentlyReading">Currently Reading</option>
                         <option value="wantToRead">Want to Read</option>
@@ -35,7 +36,7 @@ class BookComponent extends Component {
                 </div>
             </div>
             <div className="book-title">{title}</div>
-            <div className="book-authors">{authors}</div>
+            <div className="book-authors">{authors.join(', ')}</div>
         </div>
     }
 }
--- src/BooksAPI.js
+++ src/BooksAPI.js
@@ -1,5 +1,6 @@
-/** this page has a little API overview. */
+/** see this page for a little API overview. */
 const api = "https://reactnd-books-api.udacity.com";
+
 
 // Generate a unique token for storing your bookshelf data on the backend server.
 let token = localStorage.token;
